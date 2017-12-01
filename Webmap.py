c6c2f274d19184fe285152121ab33ed997148ce1
Liat Zimmermann <33725267+liatzim@users.noreply.github.com>
2017-12-01 17:01:34 +0200

    Add files via upload

---
+++ Webmap.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  1 14:14:35 2017
+
+@author: liat
+"""
+
+# Creating the interactive map using the folium function
+# The map is created through the OpenStreetMap service
+# Identifying the active volcanoes in the US
+# Displaying pop up markers throughout the map 
+
+import folium
+import pandas
+
+dir(folium)
+
+
+data = pandas.read_csv("Volcanoes_USA.txt")
+# Turn variables into lists
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
+map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Mapbox Bright")
+
+fgv = folium.FeatureGroup(name="Volcanoes")
+
+for lt, ln, el in zip(lat, lon, elev):
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius = 6, popup=str(el)+" m",
+    fill_color=color_producer(el), fill=True,  color = 'grey', fill_opacity=0.7))
+
+fgp = folium.FeatureGroup(name="Population")
+
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
+else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
+
+map.save("Map1.html")
+
+'''LAMBDA  is a function in one line, doesnt need to have a name'''
